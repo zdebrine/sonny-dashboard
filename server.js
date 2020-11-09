5373b1e71d6da1b96b01da8fdf4e975b6a99419a
Zak DeBrine <zdebrine@gmail.com>
2020-11-08 21:30:50 -0700

    Added route for adding a beacon via the API

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.get("/locations", (req, res) => {
         console.log("There was an error getting the requested info", err);
         res.send();
       } else {
+        console.log(data);
         res.status(200).send(data);
       }
     }
@@ -36,24 +37,30 @@ app.get("/locations", (req, res) => {
 });
 
 app.get("/visits/:place_id", (req, res) => {
-  if (req.params.place_id !== 'ALL') {
-    pool.query(`SELECT * FROM visits WHERE place_id = '${req.params.place_id}' AND event = 'Location Event - Enter' ORDER BY time DESC LIMIT 200`, (err, data) => {
-      if (err) {
-        console.log('Error getting visit data for one location');
-        res.send();
-      } else {
-        res.send(data);
+  if (req.params.place_id !== "ALL") {
+    pool.query(
+      `SELECT * FROM visits WHERE place_id = '${req.params.place_id}' AND event = 'Location Event - Enter' ORDER BY time DESC LIMIT 200`,
+      (err, data) => {
+        if (err) {
+          console.log("Error getting visit data for one location");
+          res.send();
+        } else {
+          res.send(data);
+        }
       }
-    });
+    );
   } else {
-    pool.query(`SELECT * FROM visits WHERE event = 'Location Event - Enter' ORDER BY time DESC LIMIT 200`, (err, data) => {
-      if (err) {
-        console.log('Error getting visit data for one location');
-        res.send();
-      } else {
-        res.send(data);
+    pool.query(
+      `SELECT * FROM visits WHERE event = 'Location Event - Enter' ORDER BY time DESC LIMIT 200`,
+      (err, data) => {
+        if (err) {
+          console.log("Error getting visit data for one location");
+          res.send();
+        } else {
+          res.send(data);
+        }
       }
-    });
+    );
   }
 });
 
@@ -86,7 +93,6 @@ app.post("/locations", (req, res) => {
 // UPDATES EDDYSTONE OR RUUVI BEACON NICKNAME AND COORDINATES IF IN DATABASE, OTHERWISE ADDS NEW BEACON
 
 app.post("/sensor", (req, res) => {
-  console.log(req.body);
   let name = req.body.beacon;
   let rssi = req.body.data.rssi;
   let humidity = req.body.data.humidity;
@@ -97,27 +103,65 @@ app.post("/sensor", (req, res) => {
   let accz = req.body.data.accelerationZ;
   let battery = req.body.data.battery;
 
-  pool.query(
-    `UPDATE "sensorData"
-      SET rssi = ${rssi}, 
-      temp = ${temp}, 
-      humidity = ${humidity}, 
-      pressure = ${pressure}, 
-      accelerationx = ${accx}, 
-      accelerationy = ${accy}, 
-      accelerationz = ${accz}, 
-      batery = ${battery} 
-     WHERE name = '${name}'`,
-    (err, data) => {
-      if (err) {
-        console.log("There was an error", err);
-        res.send(err);
+  // QUERY DB FOR SENSOR BEING POSTED
+  // IF AVAILABLE
+  // UPDATE SENSOR
+  // IF ERR
+  // ADD SENSOR TO THE DATABASE
+
+  pool.query(`SELECT * from "sensorData" WHERE name = '${name}'`, (err, data) => {
+    if (err) {
+      console.log('Error in finding beacon', err);
+      res.send(err);
+    } else {
+      if (data.rowCount === 0) {
+        pool.query(
+          `INSERT INTO "sensorData"(
+          name,
+          rssi,
+          humidity,
+          pressure,
+          temp,
+          accelerationx,
+          accelerationy,
+          accelerationz,
+          batery
+            ) VALUES ('${name}', ${rssi}, ${humidity}, ${pressure}, ${temp}, ${accx}, ${accy}, ${accz}, ${battery})`,
+          (error, response) => {
+            if (error) {
+              console.log(error);
+              res.send(error);
+            } else {
+              console.log("NEW SENSOR ADDED");
+              res.send(response);
+            }
+          }
+        );
       } else {
-        console.log("Posted to db");
-        res.status(200).send(data);
+        pool.query(
+          `UPDATE "sensorData"
+            SET rssi = ${rssi}, 
+            temp = ${temp}, 
+            humidity = ${humidity}, 
+            pressure = ${pressure}, 
+            accelerationx = ${accx}, 
+            accelerationy = ${accy}, 
+            accelerationz = ${accz}, 
+            batery = ${battery} 
+           WHERE name = '${name}'`,
+          (err, data) => {
+            if (err) {
+              console.log("There was an error", err);
+              res.send(err);
+            } else {
+              console.log("UPDATED SENSOR");
+              res.status(200).send(data);
+            }
+          }
+        );
       }
     }
-  );
+  });
 });
 
 app.get("/sensor", (req, res) => {
@@ -127,7 +171,7 @@ app.get("/sensor", (req, res) => {
     } else {
       res.status(200).send(data);
     }
-  })
+  });
 });
 
 app.listen(PORT, () => {
